c7168e24229750aa8ff0ce72df5107be4cfa63f4
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-10-24 09:20:21 +0000

    Refactored

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const el = element => document.querySelector(element)
+const el = element => document.querySelector(element);
 let todos = [], count = 0;
 
 const addTodo = () => {
   todos.push({ id:count++, text:el('#todo-input').value });
-  el('#todos-container').innerHTML = renderTodoList();
-  const todosList = el('#todos-container > ul');
-  todosList.scrollTop = todosList.scrollHeight;
+  el('#container').innerHTML = renderTodoList();
+  el('#container > ul').scrollTop = el('#container > ul').scrollHeight;
   el('#reset-button').disabled = false;
 };
 
 const deleteTodo = (id) => {
   todos = id === 'all' ? [] : todos.filter(item => item.id !== id);
-  el('#todos-container').innerHTML = renderTodoList();
+  el('#container').innerHTML = renderTodoList();
   if (todos.length === 0) {
     el('#reset-button').disabled = true;
     count = 0;
@@ -20,7 +19,7 @@ const deleteTodo = (id) => {
 
 const renderTodoList = () => {
   return '<ul>\n' + todos.map((item, index) => {
-    return `<li><span><b>${Number(item.id + 1)}</b>. ${item.text}</span><button onclick="deleteTodo(${item.id})">×</button></li>`;
+    return `\t\n<li><span><b>${Number(item.id + 1)}</b>. ${item.text}</span><button onclick="deleteTodo(${item.id})">×</button></li>`;
   }).join(''); + '\n</ul>';
 }
 
