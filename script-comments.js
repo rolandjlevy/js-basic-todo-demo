4c81c79bf46868245933c08e1b19e6194bc08e13
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-10-24 07:16:47 +0000

    Refactored

--- script-comments.js
+++ script-comments.js
@@ -1,17 +1,26 @@
+// assign variables to DOM elements
 const el = element => document.querySelector(element)
+
+// data for the todos
 let todos = [];
+
+// counter for the ids
 let count = 0;
 
+// generate the html for the list of todos
 const renderTodoList = () => {
+  // generate and return a string by looping through the todos array
   let html = '<ul>\n';
-  html += todos.map((item, index) => {
+  todos.forEach((item, index) => {
+    // delete button with id passed to the deleteTodo function 
     const deleteBtn = `<button onclick="deleteTodo(${item.id})">×</button>`;
-    return `<li><span><b>${Number(item.id + 1)}</b>. ${item.text}</span>${deleteBtn}</li>`;
-  }).join('');
-  html += '\n</ul>';
+    html += `\t<li><span><b>${Number(item.id + 1)}</b>. ${item.text}</span>${deleteBtn}</li>\n`;
+  });
+  html += '</ul>';
   return html;
 }
 
+// add a todo to the todo array then rerender the todos container
 const addTodo = () => {
   todos.push({ id:count++, text:el('#todo-input').value });
   el('#todos-container').innerHTML = renderTodoList();
@@ -19,6 +28,7 @@ const addTodo = () => {
   el('#reset-button').disabled = false;
 };
 
+// Remove all of the todos then rerender
 const resetTodos = () => {
   todos = [];
   el('#todos-container').innerHTML = renderTodoList();
@@ -26,6 +36,7 @@ const resetTodos = () => {
   count = 0;
 };
 
+// Delete a single todo then rerender
 const deleteTodo = (id) => {
   todos = todos.filter(item => item.id !== id);
   el('#todos-container').innerHTML = renderTodoList();
@@ -35,11 +46,13 @@ const deleteTodo = (id) => {
   }
 }
 
+// make the scrollbar move to the end of the todos container
 const scrollToLast = () => {
   const todosList = el('#todos-container > ul');
   todosList.scrollTop = todosList.scrollHeight;
 }
 
+// Enable or disable the add button
 const validate = (element) => {
   el('#add-button').disabled = element.value.length === 0;
 }
